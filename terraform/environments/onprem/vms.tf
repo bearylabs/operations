5beeebc2a9b5927a79c097cbb0949b588be9d5ce
bearylabs <info@beary.cloud>
2026-05-10 12:31:22 +0200

    New terraform state

--- terraform/environments/onprem/vms.tf
+++ terraform/environments/onprem/vms.tf
@@ -4,13 +4,13 @@ locals {
   dns_server     = "10.8.10.50"
 
   workers = {
-    "worker-1" = { vm_id = 201, ip = "10.8.10.71" }
-    "worker-2" = { vm_id = 202, ip = "10.8.10.72" }
+    "worker-01" = { vm_id = 201, ip = "10.8.10.71" }
+    "worker-02" = { vm_id = 202, ip = "10.8.10.72" }
   }
 }
 
 resource "proxmox_virtual_environment_vm" "server" {
-  name      = "${var.cluster_name}-server"
+  name      = "${var.cluster_name}-cp-01"
   node_name = var.proxmox_node
   vm_id     = 200
 
@@ -124,9 +124,10 @@ resource "proxmox_virtual_environment_vm" "workers" {
 
 resource "local_file" "ansible_inventory" {
   content = templatefile("${path.module}/templates/inventory.tftpl", {
-    server_ip  = "10.8.10.70"
-    worker_ips = [for k, v in local.workers : v.ip]
-    ssh_user   = "ubuntu"
+    cluster_name = var.cluster_name
+    server_ip    = "10.8.10.70"
+    worker_ips   = [for k, v in local.workers : v.ip]
+    ssh_user     = "ubuntu"
   })
   filename        = "${path.module}/../../../ansible/inventory/onprem/hosts.ini"
   file_permission = "0644"
